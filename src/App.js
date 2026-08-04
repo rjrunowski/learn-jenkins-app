8b4e2c834be6ef5561e4f3d786acbe469ddc5b6e
rjrunowski <rick.runowski@acig.com>
2026-08-04 10:08:28 -0500

    fixing build error.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://example.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My broken test site.
+        </a>
+      </header>
       <p>
-          My broken application.
+          Application version: 1
       </p>
     </div>
   );
